67936fe400eb0526750a605bb19cd93833c90122
Ashish7129 <31507313+Ashish7129@users.noreply.github.com>
2018-08-01 01:40:28 +0530

    Update SRW_RWF.py

--- sampling/SRW_RWF.py
+++ sampling/SRW_RWF.py
@@ -18,7 +18,6 @@ class SRW_RWF():
         self.fly_back_prob = 0.15
 
     def random_walk_sampling_simple(self,complete_graph, nodes_to_sample):
-        print "Sampling method: Simple Random Walk Sampling "
         nr_nodes = len(complete_graph.nodes())
         upper_bound_nr_nodes_to_sample = nodes_to_sample
         index_of_first_random_node = random.randint(0, nr_nodes-1)
@@ -45,7 +44,6 @@ class SRW_RWF():
         return sampled_graph
 
     def random_walk_sampling_with_fly_back(self,complete_graph, nodes_to_sample, fly_back_prob):
-        print "Sampling Method: Random Walk sampling With Fly Back"
         nr_nodes = len(complete_graph.nodes())
         upper_bound_nr_nodes_to_sample = nodes_to_sample
 
@@ -71,7 +69,6 @@ class SRW_RWF():
             if iteration % self.T == 0:
                 if ((sampled_graph.number_of_edges() - edges_before_t_iter) < self.growth_size):
                     curr_node = random.randint(0, nr_nodes-1)
-                    print "Choosing another random node to continue random walk "
                 edges_before_t_iter = sampled_graph.number_of_edges()
 
         return sampled_graph
