60d512d5ec9d3b626bf172f8f0d2951382100846
kirtiJain25 <41969054+kirtiJain25@users.noreply.github.com>
2018-08-01 23:57:58 +0530

    Update SRW_RWF.py

--- sampling/SRW_RWF.py
+++ sampling/SRW_RWF.py
@@ -18,9 +18,12 @@ class SRW_RWF():
         self.fly_back_prob = 0.15
 
     def random_walk_sampling_simple(self,complete_graph, nodes_to_sample):
+        print "Sampling method: Simple Random Walk Sampling "
         complete_graph = nx.convert_node_labels_to_integers(complete_graph, 0, 'default', True)
-        for n, data in g.nodes(data=True):
-            complete_graph.node[n]['id']= n
+        # giving unique id to every node same as built-in function id
+        for n, data in complete_graph.nodes(data=True):
+            complete_graph.node[n]['id'] = n
+
         nr_nodes = len(complete_graph.nodes())
         upper_bound_nr_nodes_to_sample = nodes_to_sample
         index_of_first_random_node = random.randint(0, nr_nodes-1)
@@ -47,9 +50,12 @@ class SRW_RWF():
         return sampled_graph
 
     def random_walk_sampling_with_fly_back(self,complete_graph, nodes_to_sample, fly_back_prob):
+        print "Sampling Method: Random Walk sampling With Fly Back"
         complete_graph = nx.convert_node_labels_to_integers(complete_graph, 0, 'default', True)
-        for n, data in g.nodes(data=True):
-            complete_graph.node[n]['id']= n
+        # giving unique id to every node same as built-in function id
+        for n, data in complete_graph.nodes(data=True):
+            complete_graph.node[n]['id'] = n
+
         nr_nodes = len(complete_graph.nodes())
         upper_bound_nr_nodes_to_sample = nodes_to_sample
 
@@ -75,7 +81,40 @@ class SRW_RWF():
             if iteration % self.T == 0:
                 if ((sampled_graph.number_of_edges() - edges_before_t_iter) < self.growth_size):
                     curr_node = random.randint(0, nr_nodes-1)
+                    print "Choosing another random node to continue random walk "
                 edges_before_t_iter = sampled_graph.number_of_edges()
 
         return sampled_graph
 
+    def random_walk_induced_graph_sampling(self, complete_graph, nodes_to_sample):
+        print "Random Walk Sampling with Induced Bias"
+        complete_graph = nx.convert_node_labels_to_integers(complete_graph, 0, 'default', True)
+        # giving unique id to every node same as built-in function id
+        for n, data in complete_graph.nodes(data=True):
+            complete_graph.node[n]['id'] = n
+            
+        nr_nodes = len(complete_graph.nodes())
+        upper_bound_nr_nodes_to_sample = nodes_to_sample
+        index_of_first_random_node = random.randint(0, nr_nodes - 1)
+
+        Sampled_nodes = set([complete_graph.node[index_of_first_random_node]['id']])
+
+        iteration = 1
+        nodes_before_t_iter = 0
+        curr_node = index_of_first_random_node
+        while len(Sampled_nodes) != upper_bound_nr_nodes_to_sample:
+            edges = [n for n in complete_graph.neighbors(curr_node)]
+            index_of_edge = random.randint(0, len(edges) - 1)
+            chosen_node = edges[index_of_edge]
+            Sampled_nodes.add(complete_graph.node[chosen_node]['id'])
+            curr_node = chosen_node
+            iteration=iteration+1
+
+            if iteration % self.T == 0:
+                if ((len(Sampled_nodes) - nodes_before_t_iter) < self.growth_size):
+                    curr_node = random.randint(0, nr_nodes - 1)
+                nodes_before_t_iter = len(Sampled_nodes)
+
+        sampled_graph = complete_graph.subgraph(Sampled_nodes)
+
+        return sampled_graph
