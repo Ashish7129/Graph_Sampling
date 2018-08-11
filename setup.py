bcf1cad9633f071ed98fd9ab44e14486de24db36
Ashish7129 <31507313+Ashish7129@users.noreply.github.com>
2018-08-11 13:34:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Sampling",
+    name="Graph Sampling",
     version="0.0.1",
     author="Ashish Aggarwal",
     author_email="ashish.mcs16.du@gmail.com",
