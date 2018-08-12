ddad30d866fa943b5681f2404e83b76144eed3b4
Ashish7129 <31507313+Ashish7129@users.noreply.github.com>
2018-08-12 23:46:08 +0530

    Update __init__.py

--- sampling/__init__.py
+++ sampling/__init__.py
@@ -1,8 +1,8 @@
 from SRW_RWF_ISRW import SRW_RWF_ISRW
-from Snowball import Snowball
+from Snowball import Snowball, Queue
 from ForestFire import ForestFire
 from MHRW import MHRW
 from TIES import TIES
-from Snowball import Queue
+
 
 
