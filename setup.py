4cfbb355bac2c6dfffabd787e13ea903000d2bad
Ashish7129 <31507313+Ashish7129@users.noreply.github.com>
2019-04-15 01:57:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r",encoding="utf8") as fh:
 setuptools.setup(
     name="Graph Sampling",
     version="0.0.1",
-    author="Ashish Aggarwal",
-    author_email="ashish.mcs16.du@gmail.com",
+    authors=["Kirti Jain","Ashish Aggarwal"],
+    authors_email=["kirtij.mcs16.du@gmail.com","ashish.mcs16.du@gmail.com"],
     description="Graph Sampling Package",
     long_description="A Graph Sampling package containing various approaches which samples the original graph according to different sample sizes",
     long_description_content_type="text/markdown",
