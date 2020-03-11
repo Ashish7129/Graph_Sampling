d1694f257e96524aac67863e49ae9686866d6b68
Alessio Martino <alessio.martino@uniroma1.it>
2020-03-11 17:21:22 +0100

    Linter on SRW_RWF_ISRW.py

--- Graph_Sampling/SRW_RWF_ISRW.py
+++ Graph_Sampling/SRW_RWF_ISRW.py
@@ -1,23 +1,24 @@
 import random
-import time
-import datetime
-import io
-import array,re,itertools
+# import time
+# import datetime
+# import io
+# import array, re, itertools
 import numpy as np
 import networkx as nx
-import matplotlib.pyplot as plt
-from itertools import groupby
+# import matplotlib.pyplot as plt
+# from itertools import groupby
+
 
 class SRW_RWF_ISRW:
 
     def __init__(self):
         self.growth_size = 2
-        self.T = 100 #number of iterations
-        #with a probability (1-fly_back_prob) select a neighbor node
-        #with a probability fly_back_prob go back to the initial vertex
+        self.T = 100    # number of iterations
+        # with a probability (1-fly_back_prob) select a neighbor node
+        # with a probability fly_back_prob go back to the initial vertex
         self.fly_back_prob = 0.15
 
-    def random_walk_sampling_simple(self,complete_graph, nodes_to_sample):
+    def random_walk_sampling_simple(self, complete_graph, nodes_to_sample):
         complete_graph = nx.convert_node_labels_to_integers(complete_graph, 0, 'default', True)
         # giving unique id to every node same as built-in function id
         for n, data in complete_graph.nodes(data=True):
@@ -25,7 +26,7 @@ class SRW_RWF_ISRW:
 
         nr_nodes = len(complete_graph.nodes())
         upper_bound_nr_nodes_to_sample = nodes_to_sample
-        index_of_first_random_node = random.randint(0, nr_nodes-1)
+        index_of_first_random_node = random.randint(0, nr_nodes - 1)
         sampled_graph = nx.Graph()
 
         sampled_graph.add_node(complete_graph.node[index_of_first_random_node]['id'])
@@ -40,15 +41,15 @@ class SRW_RWF_ISRW:
             sampled_graph.add_node(chosen_node)
             sampled_graph.add_edge(curr_node, chosen_node)
             curr_node = chosen_node
-            iteration = iteration+1
+            iteration = iteration + 1
 
             if iteration % self.T == 0:
                 if ((sampled_graph.number_of_edges() - edges_before_t_iter) < self.growth_size):
-                    curr_node = random.randint(0, nr_nodes-1)
+                    curr_node = random.randint(0, nr_nodes - 1)
                 edges_before_t_iter = sampled_graph.number_of_edges()
         return sampled_graph
 
-    def random_walk_sampling_with_fly_back(self,complete_graph, nodes_to_sample, fly_back_prob):
+    def random_walk_sampling_with_fly_back(self, complete_graph, nodes_to_sample, fly_back_prob):
         complete_graph = nx.convert_node_labels_to_integers(complete_graph, 0, 'default', True)
         # giving unique id to every node same as built-in function id
         for n, data in complete_graph.nodes(data=True):
@@ -57,7 +58,7 @@ class SRW_RWF_ISRW:
         nr_nodes = len(complete_graph.nodes())
         upper_bound_nr_nodes_to_sample = nodes_to_sample
 
-        index_of_first_random_node = random.randint(0, nr_nodes-1)
+        index_of_first_random_node = random.randint(0, nr_nodes - 1)
         sampled_graph = nx.Graph()
 
         sampled_graph.add_node(complete_graph.node[index_of_first_random_node]['id'])
@@ -71,15 +72,15 @@ class SRW_RWF_ISRW:
             chosen_node = edges[index_of_edge]
             sampled_graph.add_node(chosen_node)
             sampled_graph.add_edge(curr_node, chosen_node)
-            choice = np.random.choice(['prev','neigh'], 1, p=[fly_back_prob,1-fly_back_prob])
+            choice = np.random.choice(['prev', 'neigh'], 1, p=[fly_back_prob, 1 - fly_back_prob])
             if choice == 'neigh':
                 curr_node = chosen_node
-            iteration=iteration+1
+            iteration = iteration + 1
 
             if iteration % self.T == 0:
                 if ((sampled_graph.number_of_edges() - edges_before_t_iter) < self.growth_size):
-                    curr_node = random.randint(0, nr_nodes-1)
-                    print ("Choosing another random node to continue random walk ")
+                    curr_node = random.randint(0, nr_nodes - 1)
+                    print("Choosing another random node to continue random walk ")
                 edges_before_t_iter = sampled_graph.number_of_edges()
 
         return sampled_graph
@@ -89,7 +90,7 @@ class SRW_RWF_ISRW:
         # giving unique id to every node same as built-in function id
         for n, data in complete_graph.nodes(data=True):
             complete_graph.node[n]['id'] = n
-            
+
         nr_nodes = len(complete_graph.nodes())
         upper_bound_nr_nodes_to_sample = nodes_to_sample
         index_of_first_random_node = random.randint(0, nr_nodes - 1)
@@ -105,7 +106,7 @@ class SRW_RWF_ISRW:
             chosen_node = edges[index_of_edge]
             Sampled_nodes.add(complete_graph.node[chosen_node]['id'])
             curr_node = chosen_node
-            iteration=iteration+1
+            iteration = iteration + 1
 
             if iteration % self.T == 0:
                 if ((len(Sampled_nodes) - nodes_before_t_iter) < self.growth_size):
